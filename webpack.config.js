6119e3ee7b0831e6ba8af5b0a9aec1ad8d9fc0c3
rvinokurov <r4shed@gmail.com>
2017-06-16 17:01:13 +0300

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+'use strict';
+const path = require('path');
+
+module.exports = {
+    entry: './src/app.ts',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'public')
+    },
+
+    resolve: {
+        // Add `.ts` and `.tsx` as a resolvable extension.
+        extensions: ['.ts', '.tsx', '.js'] // note if using webpack 1 you'd also need a '' in the array as well
+    },
+    module: {
+        rules: [
+            { test: /\.tsx?$/, loader: 'ts-loader' },
+
+            {
+                test: /\.styl$/,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    {
+                        loader: 'stylus-loader',
+                        options: {
+                        },
+                    },
+                ],
+            }
+        ]
+    }
+};
